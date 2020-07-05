90fab2ecaee95b134a69e026c6189d20af05cca5
PeanutButte7 <adam.barta007@gmail.com>
2020-07-05 23:26:37 +0200

    Updated rate command

--- commands/rate.js
+++ commands/rate.js
@@ -9,9 +9,12 @@ module.exports = {
 		let response;
 		const ratedItem = args[args.length - 1];
 
-		if (ratedItem.localeCompare("sobě", "cz", { sensitivity: "base" }) === 0 || ratedItem.localeCompare("Auriel", "cz", { sensitivity: "base" }) === 0) {
+		if (ratedItem.localeCompare("sobě", "cz", { sensitivity: "base" }) === 0 || ratedItem.localeCompare("Auriel", "cz", { sensitivity: "base" }) === 0 || ratedItem.localeCompare("sebe", "cz", { sensitivity: "base" }) === 0 || ratedItem.localeCompare("<@!728002398370529469>", "cz", { sensitivity: "base" }) === 0) {
 			response = "To jako chceš abych hodnotila sebe? No ty ses asi úplně zbláznil ne?";
-		} else if (randomBoolean) {
+		} else if (ratedItem === "`undefined`") {
+			response = "Počkej!... to vypadá jako kdyby ses mě snad snažil rozbít! Tohle teda hodnotit nebudu"
+		}
+		else if (randomBoolean) {
 			response = `U ${ratedItem} ${sample(localCommandConfig.complete_responses)}`;
 		} else {
 			response = `${ratedItem} ${sample(localCommandConfig.template_responses)} ${Math.floor(Math.random() * 11)}/10`;
