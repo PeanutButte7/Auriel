d5cf5928e96d313a11ca030c4d85310352cdaea2
PeanutButte7 <adam.barta007@gmail.com>
2020-07-06 16:20:05 +0200

    Added testing setting and updated README

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-**For security reasons a file with token for the bot is not included. To add it simply create a file named `config.json` in the root directory and add your token.**
+**For security reasons a file with token for the bot is not included. To add it simply create a file named `config.json` in the root directory and add your token. Also specify the id for your testing channel and if bot is in production or not. If it's not it will react only to messages sent to your testing channel.**
 ```
 {
-  "token": "your_token_here"
+  "token": "your_token_here",
+   "in_production": true,
+   "testing_channel_id": "your_testing_channel_id_here"
 }
 ```
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const Discord = require("discord.js");
-const { token } = require("./config.json");
+const { token, in_production, testing_channel_id } = require("./config.json");
 const commandConfig = require("./commandConfig.json");
 
 // create a new Discord client
@@ -24,6 +24,7 @@ client.once("ready", () => {
 
 // Runs when someone sends a message
 client.on("message", message => {
+	if (message.channel.id !== testing_channel_id && !in_production) return; // Exits if message isn't sent in testing channel and bot is in testing mode
 	if (message.author.bot) return; // Exits if message is sent by a bot
 	if (!message.content.startsWith("Auriel") && (message.mentions.users.size && message.mentions.users.first().id !== "728002398370529469")) return; // Exits if message doesn't start with prefix or mentions Auriel
 
