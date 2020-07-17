e3874aec10d5e7e0c8ec869dad3fa57852f38b4b
PeanutButte7 <adam.barta007@gmail.com>
2020-07-17 22:09:37 +0200

    Added findReview command and added ColorThief package

---
+++ commands/findReview.js
@@ -0,0 +1,78 @@
+const fetch = require("node-fetch");
+const { authorization_header } = require("../config.json");
+const Discord = require("discord.js");
+const colorThief = require("colorthief");
+
+module.exports = {
+	name: "findReview",
+	description: "Find a review from Indian API",
+	execute(message, args, localCommandConfig) {
+		async function getData(url = "") {
+			const response = await fetch(url, {
+				method: "GET",
+				headers: {
+					"Authorization": authorization_header
+				}
+			});
+
+			return response.json();
+		}
+
+		// Gets name of the game
+		let game;
+		args.forEach((arg, index) => {
+			if (arg.localeCompare("recenze", "cz", { sensitivity: "base" }) === 0){
+				game = args.slice(index + 1).join(" ");
+			}
+		});
+
+		// Returns in case no words are sent after "recenze" keyword
+		if (game === ""){
+			message.channel.send("Jakou recenzi bys chtěl najít?")
+			return;
+		}
+
+		// Gets data from the api
+		getData(`https://indian-tv.cz/auriel-api/review/${game}`).then(data => {
+			const review = data.review;
+			// Returns in case no such game was found in the API
+			if (!review){
+				message.channel.send("Promiň ale tuhle hru jsem nenašla :/ Zkus zadat její jméno přesněji, můj interdimenzionální vyhledávač občas blbne");
+				return;
+			}
+
+			let imageColor = "#fcfffa";
+			if (review.coverImg){
+				// Gets main color of the image
+				const imageColorPromise = colorThief.getColor(review.coverImg);
+				imageColorPromise.then(
+					result => {
+						console.log(result);
+						imageColor = result;
+					},
+					error => {
+						console.log(error);
+					}
+				).then(() => {
+					createEmbed(review, imageColor)
+				});
+			} else {
+				console.log("Image is undefined");
+				createEmbed(review, imageColor)
+			}
+		});
+
+		function createEmbed(review, imageColor){
+			const exampleEmbed = new Discord.MessageEmbed()
+				.setColor(imageColor)
+				.setTitle(`${review.score}/10`)
+				.setAuthor(review.game,"",review.link)
+				.setDescription(review.name)
+				.setThumbnail(review.coverImg)
+				//.setImage(review.coverImg)
+				.setFooter(`Recenzoval ${review.reviewer[0].displayname}`, review.reviewer[0].avatar);
+
+			message.channel.send(exampleEmbed);
+		}
+	},
+};
